be1afc1fa102ad89defa1cf87e0d5c7548cb9c18
CornerOfTheOval <1webb@406-imac26.tahomasd.private>
2015-05-06 08:56:02 -0700

    Updated to add Scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld{
+  private static Scanner in = new Scanner(System.in);
   public static void main(String[] args){
     System.out.println("I hate this editor");
+    System.out.print("What is your name? >");
+    String name = in.nextLine();
+    System.out.println("Goodbye "+name);
   }
 
 }
